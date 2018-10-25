d5e22c69636238366d3ecc5cf4acbce99442bd55
Edijun <edijun.design@gmail.com>
2018-10-25 11:22:34 +0700

    Initial commit

---
+++ src/main/java/com/junkodesain/baseproject/MicroserviceBaseProjectApplication.java
@@ -0,0 +1,12 @@
+package com.junkodesain.baseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceBaseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceBaseProjectApplication.class, args);
+	}
+}
